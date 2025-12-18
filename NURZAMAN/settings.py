24247d61602a265e0cf31ecce45b4f3e601b0509
zero1525 <zerom0989@gmail.com>
2025-12-18 18:40:13 +0600

    static files

--- NURZAMAN/settings.py
+++ NURZAMAN/settings.py
@@ -136,6 +136,13 @@ REST_FRAMEWORK = {
 
 STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
